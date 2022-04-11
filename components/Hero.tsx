71aa85bd38f8271d6407503c11b8de14d0f83dc7
julian <jdvs_g12@hotmail.com>
2022-04-11 18:35:19 -0500

    Actualizacion responsive colaborate

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,18 +13,6 @@ const HeroContainer = styled('div', {
   width: '100%',
 })
 
-const TextContainer = styled('div', {
-  position: 'absolute',
-  color: '$white',
-  fontSize: '$5',
-  left: '4rem',
-  bottom: '20rem',
-  alignItems: 'left',
-  width: '30rem',
-  fontWeight: '700',
-  textShadow:'0.3rem 0.3rem 1rem black',
-  zIndex: '9999'
-})
 
 export const Hero = () => {
   const [carouselActiveIndex, setCarouselActiveIndex] = useState(0);
