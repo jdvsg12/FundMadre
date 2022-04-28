d5ac9668f1c3396dc0932481089a7df2be6b7be1
julian <jdvs_g12@hotmail.com>
2022-04-27 19:30:56 -0500

    changes-carrusel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,5 +1,5 @@
 
-import { css } from "@stitches/react";
+import { css } from "../stitches.config";
 import Image from "next/image";
 import React, { Component } from "react";
 import { A11y, Keyboard, Navigation, Pagination } from 'swiper';
@@ -10,22 +10,65 @@ import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 
+
 const swiperContainer = css({
-  '& .swiper-pagination-bullets': {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    paddingRight: '1%',
-    paddingBottom: '1%',  
-  },
-  '& .swiper-pagination-bullet': {
-    width: '1.875rem',
-    height: '1.8rem',
-    backgroundColor: '$pinkMain',
-    opacity: 1,
-  },
-  '& .swiper-pagination-bullet-active': {
-    border: '4px solid $pinkMain',
-    backgroundColor: '$white',
+  variants: {
+    width: {
+      Desktop: {
+        '& .swiper-pagination-bullets': {
+          display: 'flex',
+          justifyContent: 'flex-end',
+          paddingRight: '1%',
+          paddingBottom: '1%',
+        },
+        '& .swiper-pagination-bullet': {
+          width: '1.875rem',
+          height: '1.8rem',
+          backgroundColor: '$pinkMain',
+          opacity: 1,
+        },
+        '& .swiper-pagination-bullet-active': {
+          border: '4px solid $pinkMain',
+          backgroundColor: '$white',
+        }
+      },
+      Tablet: {
+        '& .swiper-pagination-bullets': {
+          display: 'flex',
+          justifyContent: 'flex-end',
+          paddingRight: '1%',
+          paddingBottom: '1%',
+        },
+        '& .swiper-pagination-bullet': {
+          width: '1.875rem',
+          height: '1.8rem',
+          backgroundColor: '$pinkMain',
+          opacity: 1,
+        },
+        '& .swiper-pagination-bullet-active': {
+          border: '3px solid $pinkMain',
+          backgroundColor: '$white',
+        }
+      },
+      Mobile: {
+        '& .swiper-pagination-bullets': {
+          display: 'flex',
+          justifyContent: 'flex-end',
+          paddingRight: '1%',
+          paddingBottom: '1%',
+        },
+        '& .swiper-pagination-bullet': {
+          width: '1rem',
+          height: '1rem',
+          backgroundColor: '$pinkMain',
+          opacity: 1,
+        },
+        '& .swiper-pagination-bullet-active': {
+          border: '2px solid $pinkMain',
+          backgroundColor: '$white',
+        }
+      }
+    }
   }
 });
 
@@ -42,7 +85,13 @@ interface CarouselProps {
 export const Carousel = ({ setActiveSlider, sliders }: CarouselProps) => {
   return (
     <Swiper
-      className={swiperContainer()}
+      className={swiperContainer({
+          width: {
+          '@initial': 'Mobile',
+          '@bp2': 'Tablet',
+          '@bp3': 'Desktop'}
+        }
+      )}
       modules={[Keyboard, Navigation, Pagination, A11y]}
       onActiveIndexChange={({ activeIndex }) => setActiveSlider?.(activeIndex)}
       pagination={{ clickable: true }}
@@ -56,16 +105,17 @@ export const Carousel = ({ setActiveSlider, sliders }: CarouselProps) => {
       cssMode={true}
     >
       {sliders.map(({ image, title }, index) => {
-     
-     
+
+
         return (
-          <SwiperSlide key={`${title}-${index}`}>
-            <Image 
+          <SwiperSlide
+            key={`${title}-${index}`}>
+            <Image
               src={image}
               alt={title}
               layout='intrinsic'
             />
-            
+
           </SwiperSlide>
         )
       })}
